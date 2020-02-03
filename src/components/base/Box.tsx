274b5dcd63ff1bab4223029ef11ebc3bfbb81587
Kristóf Poduszló <kripod@protonmail.com>
2020-02-03 17:33:21 +0100

    feat(Box): add basic stylable props

--- src/components/base/Box.tsx
+++ src/components/base/Box.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSProperties } from 'treat';
 
 // Source: https://github.com/typescript-cheatsheets/react-typescript-cheatsheet/blob/master/ADVANCED.md#extracting-prop-types-of-a-component
 type ApparentComponentProps<
@@ -8,8 +9,20 @@ type ApparentComponentProps<
   ? JSX.LibraryManagedAttributes<C, P>
   : React.ComponentProps<C>;
 
-export interface BoxProps<C extends React.ElementType = React.ElementType> {
+export interface BoxProps<C extends React.ElementType = React.ElementType>
+  extends SpacingProps,
+    ColorProps {
   as?: C;
+}
+
+interface SpacingProps {
+  padding?: CSSProperties['padding'];
+}
+
+interface ColorProps {
+  color?: CSSProperties['color'];
+  bgColor?: CSSProperties['backgroundColor'];
+  opacity?: CSSProperties['opacity'];
 }
 
 export function Box<
@@ -18,9 +31,28 @@ export function Box<
     | React.ComponentType<C extends React.ComponentType<infer P> ? P : never>
 >({
   as,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  style,
+  padding,
+  color,
+  bgColor,
+  opacity,
   ...restProps
 }: BoxProps<C> &
   React.PropsWithoutRef<ApparentComponentProps<C>>): JSX.Element {
   const Element: React.ElementType = as || 'div';
-  return <Element {...restProps} />;
+  return (
+    <Element
+      // TODO: Use treat instead of inline styles
+      style={{
+        ...style,
+        padding,
+        color,
+        backgroundColor: bgColor,
+        opacity,
+      }}
+      {...restProps}
+    />
+  );
 }
