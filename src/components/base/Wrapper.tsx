1ad26366f908937c321d07238cbf39f6d5ea5314
Kristóf Poduszló <kripod@protonmail.com>
2020-02-04 11:04:04 +0100

    revert: proposed custom treat components

--- src/components/base/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from 'treat';
-
-import { BoxProps } from './Box';
-
-export interface WrapperProps<C extends React.ElementType = React.ElementType>
-  extends BoxProps<C> {
-  // TODO: Defaults to `auto`
-  marginInline?: CSSProperties['marginInline'];
-}
