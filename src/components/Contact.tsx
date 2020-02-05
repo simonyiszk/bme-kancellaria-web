57bd6ea87886f461bfec677436dcacb1005cb13c
Kristóf Poduszló <kripod@protonmail.com>
2020-02-05 01:26:34 +0100

    refactor: create reusable Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,64 @@
+/** @jsx jsx */
+
+import { jsx, NavLink } from 'theme-ui';
+
+export interface ContactProps
+  extends React.PropsWithoutRef<JSX.IntrinsicElements['address']> {
+  name: string;
+  jobTitle: string;
+  telephone: string;
+  email: string;
+  address: string;
+}
+
+export default function Contact({
+  name,
+  jobTitle,
+  telephone,
+  email,
+  address,
+  ...props
+}: ContactProps): JSX.Element {
+  return (
+    <address
+      itemScope
+      itemType="https://schema.org/Person"
+      sx={{ fontStyle: 'normal' }}
+      {...props}
+    >
+      <span itemProp="name">{name}</span> –{' '}
+      <span itemProp="jobTitle" sx={{ textTransform: 'lowercase' }}>
+        {jobTitle}
+      </span>
+      <br />
+      <NavLink
+        href={`tel:${telephone.replace(/\s/g, '-')}`}
+        itemProp="telephone"
+        sx={{ fontWeight: 'body' }}
+      >
+        {telephone}
+      </NavLink>
+      <br />
+      <NavLink
+        href={`mailto:${email}`}
+        itemProp="email"
+        sx={{ fontWeight: 'body' }}
+      >
+        {email}
+      </NavLink>
+      <br />
+      <NavLink
+        href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+          // Truncate building-specific data, e.g. floor and room number
+          address
+            .split(',')
+            .slice(0, 2)
+            .join(','),
+        )}`}
+        itemProp="address"
+      >
+        {address}
+      </NavLink>
+    </address>
+  );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import { Box, Heading, NavLink } from 'theme-ui';
 
+import Contact from './Contact';
+
 export default function Footer(): JSX.Element {
   return (
     <Box as="footer" bg="text" color="background">
       <Heading>Kommunikáció</Heading>
-      <address itemScope itemType="https://schema.org/Person">
-        <span itemProp="name">Kovács Klára</span> –{' '}
-        <span itemProp="jobTitle">ügyvivő szakértő</span>
-        <br />
-        <NavLink
-          href="tel:+36-1-463-3984"
-          itemProp="telephone"
-          sx={{ fontWeight: 'body' }}
-        >
-          +36 1 463 3984
-        </NavLink>
-        <br />
-        <NavLink
-          href="mailto:kancellaria.kommunikacio@mail.bme.hu"
-          itemProp="email"
-          sx={{ fontWeight: 'body' }}
-        >
-          kancellaria.kommunikacio@mail.bme.hu
-        </NavLink>
-        <br />
-        <span itemProp="address">
-          {' '}
-          1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202
-        </span>
-      </address>
+      <Contact
+        name="Kovács Klára"
+        jobTitle="Ügyvivő szakértő"
+        telephone="+36 1 463 3984"
+        email="kancellaria.kommunikacio@mail.bme.hu"
+        address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202"
+      />
 
       <Heading>Rendezvények, terembérlés</Heading>
     </Box>
