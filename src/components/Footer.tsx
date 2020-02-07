6691c7a8db128975cacd049bcfbfb0b5ec746fcf
Kristóf Poduszló <kripod@protonmail.com>
2020-02-07 13:29:16 +0100

    feat(footer): basic flex layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { jsx, Link, Styled } from 'theme-ui';
+import { Flex, jsx, Link, Styled } from 'theme-ui';
 
 import { Contact } from './Contact';
 
@@ -10,88 +10,107 @@ export type FooterProps = React.PropsWithoutRef<
 
 export function Footer(props: FooterProps): JSX.Element {
   return (
-    <footer {...props}>
-      <Styled.h2>Kommunikáció</Styled.h2>
-      <Contact
-        name="Kovács Klára"
-        jobTitle="Ügyvivő szakértő"
-        telephone="+36 1 463 3984"
-        email="kancellaria.kommunikacio@mail.bme.hu"
-        address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202"
-      />
+    <Flex
+      as="footer"
+      sx={{
+        // TODO: Horizontal padding
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+      }}
+      {...props}
+    >
+      <div>
+        <Styled.h2>Kommunikáció</Styled.h2>
+        <Contact
+          name="Kovács Klára"
+          jobTitle="Ügyvivő szakértő"
+          telephone="+36 1 463 3984"
+          email="kancellaria.kommunikacio@mail.bme.hu"
+          address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202"
+        />
 
-      <Styled.h2>Rendezvények, terembérlés</Styled.h2>
-      <Contact
-        name="Major Sándor"
-        jobTitle="Igazgatási ügyintéző"
-        telephone="+36 1 463 3868"
-        email="rendezveny@mail.bme.hu"
-        address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. HSZI"
-      />
+        <Styled.h2>Rendezvények, terembérlés</Styled.h2>
+        <Contact
+          name="Major Sándor"
+          jobTitle="Igazgatási ügyintéző"
+          telephone="+36 1 463 3868"
+          email="rendezveny@mail.bme.hu"
+          address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. HSZI"
+        />
+      </div>
 
-      <Styled.h2>Hasznos oldalak</Styled.h2>
-      <ul sx={{ variant: 'text.caps' }}>
-        <li>
-          <Link href="https://kth.bme.hu/" variant="inverted">
-            KTH
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.bme.hu/" variant="inverted">
-            BME.hu
-          </Link>
-        </li>
-        <li>
-          <Link href="https://telefon.bme.hu/" variant="inverted">
-            Telefonkönyv
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
-            MNB árfolyamok
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
-            variant="inverted"
-          >
-            Jegybanki alapkamat
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
-            variant="inverted"
-          >
-            NAV üzemanyagárak
-          </Link>
-        </li>
-      </ul>
+      <Flex sx={{ justifyContent: 'space-between' }}>
+        <div>
+          <Styled.h2>Hasznos oldalak</Styled.h2>
+          <ul sx={{ variant: 'text.caps' }}>
+            <li>
+              <Link href="https://kth.bme.hu/" variant="inverted">
+                KTH
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.bme.hu/" variant="inverted">
+                BME.hu
+              </Link>
+            </li>
+            <li>
+              <Link href="https://telefon.bme.hu/" variant="inverted">
+                Telefonkönyv
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
+                MNB árfolyamok
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
+                variant="inverted"
+              >
+                Jegybanki alapkamat
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
+                variant="inverted"
+              >
+                NAV üzemanyagárak
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-      <Styled.h2>Szolgáltatások</Styled.h2>
-      <ul sx={{ variant: 'text.caps' }}>
-        <li>
-          <Link href="https://www.omikk.bme.hu/" variant="inverted">
-            Könyvtár
-          </Link>
-        </li>
-        <li>
-          <Link href="https://net.bme.hu/" variant="inverted">
-            BMENET
-          </Link>
-        </li>
-        <li>
-          <Link href="https://muegyetemiallasborze.hu/" variant="inverted">
-            Műegyetemi Állásbörze
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.bme.hu/allaspalyazatok" variant="inverted">
-            Álláspályázatok
-          </Link>
-        </li>
-      </ul>
-    </footer>
+        <div>
+          <Styled.h2>Szolgáltatások</Styled.h2>
+          <ul sx={{ variant: 'text.caps' }}>
+            <li>
+              <Link href="https://www.omikk.bme.hu/" variant="inverted">
+                Könyvtár
+              </Link>
+            </li>
+            <li>
+              <Link href="https://net.bme.hu/" variant="inverted">
+                BMENET
+              </Link>
+            </li>
+            <li>
+              <Link href="https://muegyetemiallasborze.hu/" variant="inverted">
+                Műegyetemi Állásbörze
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.bme.hu/allaspalyazatok"
+                variant="inverted"
+              >
+                Álláspályázatok
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Flex>
+    </Flex>
   );
 }
