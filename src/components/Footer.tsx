a8f47f37dc1197e631cc7590ed26c5e790b8cd74
Kristóf Poduszló <kripod@protonmail.com>
2020-02-09 10:25:07 +0100

    refactor(footer): execute layout with flexbox

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@
 
 import { Box, jsx, Link, Styled } from 'theme-ui';
 
+import { Cluster } from './Cluster';
 import { Contact } from './Contact';
 
 export type FooterProps = React.PropsWithoutRef<
@@ -10,116 +11,127 @@ export type FooterProps = React.PropsWithoutRef<
 
 export function Footer(props: FooterProps): JSX.Element {
   return (
-    <footer
-      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-      sx={theme => ({
-        display: 'grid',
-        gridAutoFlow: [null, null, 'column'],
-        alignItems: 'center',
-        justifyContent: ['space-around', 'space-evenly'],
-        gridGap: `0 ${theme.space[32]}`,
-      })}
-      {...props}
-    >
-      <div>
-        <Styled.h2>Kommunikáció</Styled.h2>
-        <Contact
-          name="Kovács Klára"
-          jobTitle="Ügyvivő szakértő"
-          telephone="+36 1 463 3984"
-          email="kancellaria.kommunikacio@mail.bme.hu"
-          address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202"
-        />
-
-        <Styled.h2>Rendezvények, terembérlés</Styled.h2>
-        <Contact
-          name="Major Sándor"
-          jobTitle="Igazgatási ügyintéző"
-          telephone="+36 1 463 3868"
-          email="rendezveny@mail.bme.hu"
-          address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. HSZI"
-        />
-      </div>
-
-      <div
-        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-        sx={theme => ({
-          display: 'grid',
-          gridAutoFlow: [null, null, null, null, 'column'],
-          gridGap: `0 ${theme.space[24]}`,
-        })}
+    <footer sx={{ textAlign: 'center' }} {...props}>
+      <Cluster
+        columnGap={32}
+        sx={{
+          textAlign: 'initial',
+          display: 'inline-flex',
+          flexDirection: ['column', null, 'row'],
+        }}
       >
         <div>
-          <Styled.h2>Hasznos oldalak</Styled.h2>
-          <Box as="ul" variant="lists.unstyled" sx={{ variant: ['text.caps'] }}>
-            <li>
-              <Link href="https://kth.bme.hu/" variant="inverted">
-                KTH
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.bme.hu/" variant="inverted">
-                BME.hu
-              </Link>
-            </li>
-            <li>
-              <Link href="https://telefon.bme.hu/" variant="inverted">
-                Telefonkönyv
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
-                MNB árfolyamok
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
-                variant="inverted"
-              >
-                Jegybanki alapkamat
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
-                variant="inverted"
-              >
-                NAV üzemanyagárak
-              </Link>
-            </li>
-          </Box>
+          <Styled.h2>Kommunikáció</Styled.h2>
+          <Contact
+            name="Kovács Klára"
+            jobTitle="Ügyvivő szakértő"
+            telephone="+36 1 463 3984"
+            email="kancellaria.kommunikacio@mail.bme.hu"
+            address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. 202"
+          />
+
+          <Styled.h2>Rendezvények, terembérlés</Styled.h2>
+          <Contact
+            name="Major Sándor"
+            jobTitle="Igazgatási ügyintéző"
+            telephone="+36 1 463 3868"
+            email="rendezveny@mail.bme.hu"
+            address="1118 Budapest, Műegyetem rkp. 7–9., 2. em. HSZI"
+          />
         </div>
 
         <div>
-          <Styled.h2>Szolgáltatások</Styled.h2>
-          <Box as="ul" variant="lists.unstyled" sx={{ variant: ['text.caps'] }}>
-            <li>
-              <Link href="https://www.omikk.bme.hu/" variant="inverted">
-                Könyvtár
-              </Link>
-            </li>
-            <li>
-              <Link href="https://net.bme.hu/" variant="inverted">
-                BMENET
-              </Link>
-            </li>
-            <li>
-              <Link href="https://muegyetemiallasborze.hu/" variant="inverted">
-                Műegyetemi Állásbörze
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://www.bme.hu/allaspalyazatok"
-                variant="inverted"
+          <Cluster
+            columnGap={24}
+            sx={{
+              display: 'inline-flex',
+              flexDirection: ['column', null, null, null, 'row'],
+            }}
+          >
+            <div>
+              <Styled.h2>Hasznos oldalak</Styled.h2>
+              <Box
+                as="ul"
+                variant="lists.unstyled"
+                sx={{ variant: ['text.caps'] }}
               >
-                Álláspályázatok
-              </Link>
-            </li>
-          </Box>
+                <li>
+                  <Link href="https://kth.bme.hu/" variant="inverted">
+                    KTH
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.bme.hu/" variant="inverted">
+                    BME.hu
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://telefon.bme.hu/" variant="inverted">
+                    Telefonkönyv
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
+                    MNB árfolyamok
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
+                    variant="inverted"
+                  >
+                    Jegybanki alapkamat
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
+                    variant="inverted"
+                  >
+                    NAV üzemanyagárak
+                  </Link>
+                </li>
+              </Box>
+            </div>
+
+            <div>
+              <Styled.h2>Szolgáltatások</Styled.h2>
+              <Box
+                as="ul"
+                variant="lists.unstyled"
+                sx={{ variant: ['text.caps'] }}
+              >
+                <li>
+                  <Link href="https://www.omikk.bme.hu/" variant="inverted">
+                    Könyvtár
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://net.bme.hu/" variant="inverted">
+                    BMENET
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://muegyetemiallasborze.hu/"
+                    variant="inverted"
+                  >
+                    Műegyetemi Állásbörze
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.bme.hu/allaspalyazatok"
+                    variant="inverted"
+                  >
+                    Álláspályázatok
+                  </Link>
+                </li>
+              </Box>
+            </div>
+          </Cluster>
         </div>
-      </div>
+      </Cluster>
     </footer>
   );
 }
