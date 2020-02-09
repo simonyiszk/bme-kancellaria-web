cd12b4775b451b47697503ffd8feb5e823d55d75
Kristóf Poduszló <kripod@protonmail.com>
2020-02-09 01:02:02 +0100

    improvement(footer): simplify by using CSS Grid

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,23 @@
 
 import { Box, jsx, Link, Styled } from 'theme-ui';
 
-import { Cluster, ClusterProps } from './Cluster';
 import { Contact } from './Contact';
 
-export type FooterProps = ClusterProps;
+export type FooterProps = React.PropsWithoutRef<
+  JSX.IntrinsicElements['footer']
+>;
 
 export function Footer(props: FooterProps): JSX.Element {
   return (
-    <Cluster
-      as="footer"
-      columnGap={32}
-      sx={{
-        justifyContent: ['space-between', 'space-around', 'space-evenly'],
-      }}
+    <footer
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      sx={theme => ({
+        display: 'grid',
+        gridAutoFlow: [null, null, 'column'],
+        alignItems: 'center',
+        justifyContent: ['space-around', 'space-evenly'],
+        gridGap: `0 ${theme.space[32]}`,
+      })}
       {...props}
     >
       <div>
@@ -37,94 +41,85 @@ export function Footer(props: FooterProps): JSX.Element {
         />
       </div>
 
-      <div>
-        <Cluster
-          columnGap={24}
-          sx={{ flexDirection: [null, null, 'column', null, 'row'] }}
-        >
-          <div>
-            <Styled.h2>Hasznos oldalak</Styled.h2>
-            <Box
-              as="ul"
-              variant="lists.unstyled"
-              sx={{ variant: ['text.caps'] }}
-            >
-              <li>
-                <Link href="https://kth.bme.hu/" variant="inverted">
-                  KTH
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.bme.hu/" variant="inverted">
-                  BME.hu
-                </Link>
-              </li>
-              <li>
-                <Link href="https://telefon.bme.hu/" variant="inverted">
-                  Telefonkönyv
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
-                  MNB árfolyamok
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
-                  variant="inverted"
-                >
-                  Jegybanki alapkamat
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
-                  variant="inverted"
-                >
-                  NAV üzemanyagárak
-                </Link>
-              </li>
-            </Box>
-          </div>
+      <div
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        sx={theme => ({
+          display: 'grid',
+          gridAutoFlow: [null, null, null, null, 'column'],
+          gridGap: `0 ${theme.space[24]}`,
+        })}
+      >
+        <div>
+          <Styled.h2>Hasznos oldalak</Styled.h2>
+          <Box as="ul" variant="lists.unstyled" sx={{ variant: ['text.caps'] }}>
+            <li>
+              <Link href="https://kth.bme.hu/" variant="inverted">
+                KTH
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.bme.hu/" variant="inverted">
+                BME.hu
+              </Link>
+            </li>
+            <li>
+              <Link href="https://telefon.bme.hu/" variant="inverted">
+                Telefonkönyv
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.mnb.hu/arfolyamok" variant="inverted">
+                MNB árfolyamok
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.mnb.hu/Jegybanki_alapkamat_alakulasa"
+                variant="inverted"
+              >
+                Jegybanki alapkamat
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://nav.gov.hu/nav/szolgaltatasok/uzemanyag/uzemanyagarak"
+                variant="inverted"
+              >
+                NAV üzemanyagárak
+              </Link>
+            </li>
+          </Box>
+        </div>
 
-          <div>
-            <Styled.h2>Szolgáltatások</Styled.h2>
-            <Box
-              as="ul"
-              variant="lists.unstyled"
-              sx={{ variant: ['text.caps'] }}
-            >
-              <li>
-                <Link href="https://www.omikk.bme.hu/" variant="inverted">
-                  Könyvtár
-                </Link>
-              </li>
-              <li>
-                <Link href="https://net.bme.hu/" variant="inverted">
-                  BMENET
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://muegyetemiallasborze.hu/"
-                  variant="inverted"
-                >
-                  Műegyetemi Állásbörze
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://www.bme.hu/allaspalyazatok"
-                  variant="inverted"
-                >
-                  Álláspályázatok
-                </Link>
-              </li>
-            </Box>
-          </div>
-        </Cluster>
+        <div>
+          <Styled.h2>Szolgáltatások</Styled.h2>
+          <Box as="ul" variant="lists.unstyled" sx={{ variant: ['text.caps'] }}>
+            <li>
+              <Link href="https://www.omikk.bme.hu/" variant="inverted">
+                Könyvtár
+              </Link>
+            </li>
+            <li>
+              <Link href="https://net.bme.hu/" variant="inverted">
+                BMENET
+              </Link>
+            </li>
+            <li>
+              <Link href="https://muegyetemiallasborze.hu/" variant="inverted">
+                Műegyetemi Állásbörze
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.bme.hu/allaspalyazatok"
+                variant="inverted"
+              >
+                Álláspályázatok
+              </Link>
+            </li>
+          </Box>
+        </div>
       </div>
-    </Cluster>
+    </footer>
   );
 }
