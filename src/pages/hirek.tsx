c17406cdbf87d620f70297c40b60616779522a41
Andris Borbás <andris.borbas@gmail.com>
2020-06-02 23:17:48 +0200

    feat(news): Use props and create clickable link #10

--- src/pages/hirek.tsx
+++ src/pages/hirek.tsx
@@ -2,6 +2,7 @@
 
 import { Grid, jsx } from 'theme-ui';
 
+import NewsImage from '../assets/news.png';
 import { NewsArticleTeaser } from '../components/news/NewsArticleTeaser';
 
 export default function NewsPage(): JSX.Element {
@@ -17,14 +18,20 @@ export default function NewsPage(): JSX.Element {
         justifyItems: 'center',
       })}
     >
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
-      <NewsArticleTeaser />
+      {Array(9)
+        .fill(0)
+        .map(() => {
+          return (
+            <NewsArticleTeaser
+              title="Modern gyárak éjszakája a Műegyetemen"
+              summary="Az ITM által szervezett oszágos rendezvény keretében az Ipar 4.0
+            Technológiai Központ is több időpontban várja szeretettel az
+            érdeklődőket november 15-én."
+              thumbnailURL={NewsImage}
+              datePublished={{ raw: '2019-10-29', pretty: '2019. október 29.' }}
+            />
+          );
+        })}
     </Grid>
   );
 }
